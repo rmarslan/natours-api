7b404fe3b740b3d8a6df57c4a88c55b91c4d1f1a
RM Arslan <rmarslanrana@gmail.com>
2020-01-08 21:32:23 -0800

    Add config module

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const debug = require('debug')('app');
 dotenv.config({ path: './config.env' });
 const app = express();
 
+if (!process.env.NODE_ENV) {
+  debug(`FATAL ERROR: Please set the NODE_ENV environment variable.`);
+  process.exit(1);
+}
 // Middleware
 if (process.env.NODE_ENV.trim() === 'development') {
   debug('Morgan is enabled');
@@ -16,6 +20,8 @@ app.get('/', (req, res) => {
   res.status(200).send('<h1>Hello World</h1>');
 });
 
-app.listen(3000, () => {
-  debug('server is listening on port 3000...');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  debug(`Server is listening on ${port}...`);
 });
