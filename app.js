24d98be6e6a17f442abde0e7455e56bf57e77c92
RM Arslan <rmarslanrana@gmail.com>
2020-01-08 21:50:37 -0800

    Refactoring

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
-const dotenv = require('dotenv');
 const express = require('express');
 const morgan = require('morgan');
-const debug = require('debug')('app');
+const debug = require('debug')('natours:app');
 
-dotenv.config({ path: './config.env' });
 const app = express();
 
-if (!process.env.NODE_ENV) {
-  debug(`FATAL ERROR: Please set the NODE_ENV environment variable.`);
-  process.exit(1);
-}
 // Middleware
 if (process.env.NODE_ENV.trim() === 'development') {
   debug('Morgan is enabled');
@@ -20,8 +14,4 @@ app.get('/', (req, res) => {
   res.status(200).send('<h1>Hello World</h1>');
 });
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  debug(`Server is listening on ${port}...`);
-});
+module.exports = app;
