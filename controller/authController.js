2a74476b257000d08302c6e944f5131300cd7380
RM Arslan <rmarslanrana@gmail.com>
2020-01-11 02:10:35 -0800

    Send jwt token via cookey

--- controller/authController.js
+++ controller/authController.js
@@ -6,6 +6,18 @@ const sendEmail = require('./../utils/email');
 
 const createSendToken = (user, statusCode, res) => {
   const token = user.getJwtToken();
+
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true
+  };
+
+  if (process.env.NODE_ENV.trim() === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+
   user.password = undefined;
   res.status(statusCode).json({
     status: 'success',
