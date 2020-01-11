b22ac1efbe9215caeb2d6bb43afeac85ea580058
RM Arslan <rmarslanrana@gmail.com>
2020-01-11 03:12:21 -0800

    Add parameter pollution protection

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 const tourRouter = require('./routes/tourRoute');
 const userRouter = require('./routes/userRoutes');
 const errorController = require('./controller/errorController');
@@ -39,6 +40,20 @@ app.use(mongoSanitize());
 
 // Data sanitization against XSS
 app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+);
 
 // Routes
 app.use('/api/v1/tours', tourRouter);
