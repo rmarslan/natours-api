fe12231fde34b806a568d5a716fd0defcc6b619c
RM Arslan <rmarslanrana@gmail.com>
2020-01-11 03:06:14 -0800

    Add mongogo sanitizer and xss sanitizer

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const morgan = require('morgan');
 const debug = require('debug')('natours:app');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 const tourRouter = require('./routes/tourRoute');
 const userRouter = require('./routes/userRoutes');
 const errorController = require('./controller/errorController');
@@ -9,12 +12,17 @@ const errorController = require('./controller/errorController');
 const app = express();
 
 // Global Middleware
-app.use(express.json());
+
+// Set security http headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV.trim() === 'development') {
   debug('Morgan is enabled');
   app.use(morgan('dev'));
 }
 
+// Limit requests from the same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -23,6 +31,16 @@ const limiter = rateLimit({
 
 app.use(limiter);
 
+// Body parser
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against noSQL injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
